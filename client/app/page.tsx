e151b826d913fe9b243f6e1245f33548e26ff137
ChandraShekhar <csyadav0513@gmail.com>
2024-11-21 11:16:00 +0530

    Add Radix UI Slot dependency and create a customizable Button component; update homepage layout

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,35 +1,33 @@
+import { Cover } from "@/components/ui/cover";
+
 export default function Home() {
   return (
     <>
-      <section className="relative bg-[url(https://images.unsplash.com/photo-1604014237800-1c9102c219da?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80)] bg-cover bg-center bg-no-repeat">
-        <div className="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"></div>
+      <section className="bg-gray-50">
+        <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
+          <div className="mx-auto text-center">
+            <div>
+              <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+                Build amazing websites <br /> at <Cover>warp speed</Cover>
+              </h1>
+            </div>
 
-        <div className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8">
-          <div className="max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
-            <h1 className="text-3xl font-extrabold text-white  sm:text-5xl">
-              Let us find your
-              <strong className="block font-extrabold text-rose-500">
-                {" "}
-                Forever Home.{" "}
-              </strong>
-            </h1>
-
-            <p className="mt-4 max-w-lg text-white sm:text-xl/relaxed">
+            <p className="mt-4 sm:text-xl/relaxed">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
               illo tenetur fuga ducimus numquam ea!
             </p>
 
-            <div className="mt-8 flex flex-wrap gap-4 text-center">
+            <div className="mt-8 flex flex-wrap justify-center gap-4">
               <a
+                className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
                 href="#"
-                className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
               >
                 Get Started
               </a>
 
               <a
+                className="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
                 href="#"
-                className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
               >
                 Learn More
               </a>
