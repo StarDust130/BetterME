90560debf8c92786855b1669b5decb239113210b
ChandraShekhar <csyadav0513@gmail.com>
2024-11-21 18:51:33 +0530

    Refactor Navbar component styles for improved clarity and consistency; remove background color changes on active links

--- client/components/elements/Navbar.tsx
+++ client/components/elements/Navbar.tsx
@@ -7,20 +7,20 @@ function Navbar() {
   const [activeNav, setActiveNav] = useState("/home");
 
   return (
-    <nav className="fixed bottom-0 left-0 w-full h-[60px] sm:h-auto md:h-[80px] bg-white dark:bg-neutral-950 text-black dark:text-white flex items-center justify-between px-4 sm:justify-around sm:w-auto sm:rounded-full sm:bottom-4 sm:left-1/2 sm:transform sm:-translate-x-1/2 z-50 shadow-md md:shadow-lg border-t-2 sm:border-2 border-solid border-neutral-300 dark:border-neutral-700 transition-all md:gap-8 md:px-6 lg:px-12 md:rounded-xl md:max-w-4xl lg:max-w-6xl">
+    <nav className="fixed bottom-0 left-0 w-full h-[60px] sm:h-auto  bg-white dark:bg-neutral-950 text-black dark:text-white flex items-center justify-between px-4 sm:justify-around sm:w-auto sm:rounded-full sm:bottom-4 sm:left-1/2 sm:transform sm:-translate-x-1/2 z-50 shadow-md md:shadow-lg border-t-2 sm:border-2 border-solid border-neutral-300 dark:border-neutral-700 transition-all md:gap-8 md:px-6 lg:px-12 md:rounded-full md:max-w-4xl lg:max-w-6xl">
       {/* Home Link */}
       <Link
         href="/home"
         onClick={() => setActiveNav("/home")}
         className={`flex flex-col md:flex-row items-center justify-center w-full sm:w-auto p-2 sm:p-3 md:p-4 transition-colors ${
           activeNav === "/home"
-            ? "bg-neutral-200 dark:bg-neutral-800 rounded-2xl text-black dark:text-white"
-            : "text-gray-600 dark:text-gray-400 hover:bg-neutral-100 dark:hover:bg-neutral-700"
+            ? " rounded-2xl text-black dark:text-white"
+            : "text-gray-600 dark:text-gray-400 "
         }`}
       >
         <Home
           className={`h-5 w-5 sm:h-6 sm:w-6 md:h-8 md:w-8 ${
-            activeNav === "/home" ? "fill-transparent" : ""
+            activeNav === "/home" ? "brightness-200" : ""
           }`}
         />
         <span className="text-[10px] sm:text-sm md:text-base mt-1 sm:mt-2 md:mt-0 md:ml-2">
@@ -34,8 +34,8 @@ function Navbar() {
         onClick={() => setActiveNav("/create")}
         className={`flex flex-col md:flex-row rounded-2xl items-center justify-center w-full sm:w-auto p-2 sm:p-3 md:p-4 transition-colors ${
           activeNav === "/create"
-            ? "bg-neutral-200 dark:bg-neutral-800  text-black dark:text-white"
-            : "text-gray-600 dark:text-gray-400 hover:bg-neutral-100 dark:hover:bg-neutral-700"
+            ? "  text-black dark:text-white"
+            : "text-gray-600 dark:text-gray-400 "
         }`}
       >
         <CirclePlus
@@ -54,8 +54,8 @@ function Navbar() {
         onClick={() => setActiveNav("/stats")}
         className={`flex flex-col md:flex-row rounded-2xl items-center justify-center w-full sm:w-auto p-2 sm:p-3 md:p-4 transition-colors ${
           activeNav === "/stats"
-            ? "bg-neutral-200 dark:bg-neutral-800  text-black dark:text-white"
-            : "text-gray-600 dark:text-gray-400 hover:bg-neutral-100 dark:hover:bg-neutral-700"
+            ? "  text-black dark:text-white"
+            : "text-gray-600 dark:text-gray-400 "
         }`}
       >
         <ChartNoAxesCombined
