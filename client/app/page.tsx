4f82ac1a71810b3c6c26c10a55e12c094be02236
ChandraShekhar <csyadav0513@gmail.com>
2024-11-21 11:38:06 +0530

    Refactor global styles with CSS variables for theming; update homepage layout and button components

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,36 +1,32 @@
+import { Button } from "@/components/ui/button";
 import { Cover } from "@/components/ui/cover";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <section className="bg-gray-50">
+      <section>
         <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
           <div className="mx-auto text-center">
             <div>
-              <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
-                Build amazing websites <br /> at <Cover>warp speed</Cover>
+              <h1 className="text-4xl md:text-4xl lg:text-6xl font-semibold max-w-7xl mx-auto mt-6 py-6 bg-clip-text text-transparent bg-gradient-to-b from-blue-600 via-red-400 to-yellow-100">
+                Transform yourself effortlessly, <br />{" "}
+                <Cover>one habit at a time! </Cover>
               </h1>
             </div>
 
-            <p className="mt-4 sm:text-xl/relaxed">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-              illo tenetur fuga ducimus numquam ea!
+            <p className="mt-4 sm:text-xl">
+              Improve your daily habits and achieve your goals effortlessly with
+              BetterMe!
             </p>
 
             <div className="mt-8 flex flex-wrap justify-center gap-4">
-              <a
-                className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
-                href="#"
-              >
-                Get Started
-              </a>
-
-              <a
-                className="block w-full rounded px-12 py-3 text-sm font-medium text-red-600 shadow hover:text-red-700 focus:outline-none focus:ring active:text-red-500 sm:w-auto"
-                href="#"
-              >
-                Learn More
-              </a>
+              <Button asChild size={"lg"}>
+                <Link href="/home">Get Started</Link>
+              </Button>
+              <Button variant={"outline"} asChild size={"lg"}>
+                <Link href="/home">Learn More</Link>
+              </Button>
             </div>
           </div>
         </div>
