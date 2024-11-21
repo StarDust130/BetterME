35b7a54a08021a248a3e2f7012275200a00b1a88
ChandraShekhar <csyadav0513@gmail.com>
2024-11-21 19:41:42 +0530

    Remove unused Create page component; enhance Home page with user greeting and daily quote display; update global styles for improved theming; configure Next.js for remote image loading

--- client/app/(root)/home/page.tsx
+++ client/app/(root)/home/page.tsx
@@ -1,11 +1,29 @@
 import { currentUser } from "@clerk/nextjs/server";
 
-const page = async () => {
+const Page = async () => {
   const user = await currentUser();
+
+  // Fetch quotes directly
+  const response = await fetch("https://zenquotes.io/api/today");
+  const data = await response.json();
+  const quote = data[0]; // Access the first item in the array
+
   return (
-    <>
-      <h1 className=" text-2xl  mx-auto w-full text-center md:text-4xl">Hello 😊, {user?.firstName}</h1>
-    </>
+    <main className="flex flex-col  items-center w-full  p-4">
+      <h1 className="text-2xl font-bold text-center mb-6 md:text-4xl">
+        Hello 😊, {user?.firstName || "Guest"}
+      </h1>
+
+      <div className="bg-gray-100 dark:bg-neutral-800  max-w-3xl w-full p-6 rounded-lg shadow-lg">
+        <blockquote className="text-xl md:text-2xl italic font-light text-center">
+          &quot;{quote.q}&quot;
+        </blockquote>
+        <p className="mt-4 text-right text-lg md:text-xl font-semibold text-primary">
+          - {quote.a}
+        </p>
+      </div>
+    </main>
   );
 };
-export default page;
+
+export default Page;
