3babec9657f021aca3127d6f7c9c5541a6240640
ChandraShekhar <csyadav0513@gmail.com>
2024-11-23 10:20:24 +0530

    Add loading spinner component and styles for loading state

---
+++ client/app/loading.tsx
@@ -0,0 +1,16 @@
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center gap-2 w-full h-screen flex-col">
+      <div className="spinner">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="text-sm md:text-2xl animate-pulse">Loading...</div>
+    </div>
+  );
+};
+export default Loading;
