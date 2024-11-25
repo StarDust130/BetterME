25ccbb9d936623edbf86814686eb4444e6ebe181
ChandraShekhar <csyadav0513@gmail.com>
2024-11-25 13:09:01 +0530

    Update validation message for title in expenses schema

--- client/lib/zodSchema.ts
+++ client/lib/zodSchema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const expensesSchema = z.object({
   title: z
     .string()
-    .min(2, { message: "Username must be at least 2 characters." })
+    .min(2, { message: "Title must be at least 2 characters." })
     .max(50),
 
   amount: z.number({ message: "Amount is Required 😅" }).int().positive(),
