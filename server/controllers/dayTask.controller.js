f0907ebb2ada6d0206cff97516f6a9889d2d940e
ChandraShekhar <csyadav0513@gmail.com>
2024-12-03 12:07:31 +0530

    Add DayTask controller methods for creating and retrieving daily tasks

--- server/controllers/dayTask.controller.js
+++ server/controllers/dayTask.controller.js
@@ -1,2 +1,52 @@
-import DayTask from "../models/dayTask.models.js"
+import DayTask from "../models/dayTask.models.js";
+import { AppError } from "../lib/AppError.js";
+import { catchAsync } from "../lib/catchAsync.js";
 
+//! Get 🐘
+const getDayTask = catchAsync(async (req, res, next) => {
+  // 1) Get Data from Query
+  const { clerkID } = req.query;
+
+  if (!clerkID) {
+    return next(new AppError("Please Provide Clerk ID", 400));
+  }
+
+  // 2) Fetch from DB
+  const dayTask = await DayTask.findOne({ clerkID, date });
+
+  // 3) Send Response
+  res.status(200).json({
+    status: "success",
+    data: {
+      dayTask,
+    },
+  });
+});
+
+//! Create 🐧
+const createDayTask = catchAsync(async (req, res, next) => {
+  // 1) Get Data from Body
+  const { clerkID, date, tasks } = req.body;
+
+  if (!clerkID) {
+    return next(new AppError("Please Provide Clerk ID", 400));
+  }
+
+  // 2) Save to DB
+  const dayTask = await DayTask.create({
+    clerkID,
+    date,
+    tasks,
+  });
+
+  // 3) Send Response
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      dayTask,
+    },
+  });
+});
+
+export { createDayTask , getDayTask };
