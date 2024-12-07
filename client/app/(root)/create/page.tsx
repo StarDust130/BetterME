ced40a0ee9c145c80add5391c831c3c15ba7ef8e
ChandraShekhar <csyadav0513@gmail.com>
2024-12-07 12:29:07 +0530

    Update grid layout in create page for improved responsiveness

--- client/app/(root)/create/page.tsx
+++ client/app/(root)/create/page.tsx
@@ -17,7 +17,7 @@ const Page = () => {
       </p>
 
       {/* Cards Container */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 w-full max-w-3xl">
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 w-full max-w-3xl md:max-w-5xl">
         {cardData.map((card) => (
           <Card data={card} key={card.id} />
         ))}         
