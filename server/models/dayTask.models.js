b1cd1e07781c28376c2c161375ad3cff6cfd686c
ChandraShekhar <csyadav0513@gmail.com>
2024-12-08 11:45:38 +0530

    Remove required validation from journal text field in dayTask model

--- server/models/dayTask.models.js
+++ server/models/dayTask.models.js
@@ -21,7 +21,7 @@ const dayTaskSchema = new mongoose.Schema(
       },
     ],
     journal: {
-      text: { type: String, min: 2, required: true },
+      text: { type: String },
     },
     todo: [
       {
