178efd0c3a2671240021b8a302a96053d4eb0de3
ChandraShekhar <csyadav0513@gmail.com>
2024-12-10 13:47:01 +0530

    Refactor List component to use NoDataFound for empty states and improve layout for Today's Activity section

--- client/components/elements/List.tsx
+++ client/components/elements/List.tsx
@@ -128,7 +128,7 @@ const List = () => {
                 </div>
               ) : (
                 <div className="flex flex-col items-center justify-center py-4">
-                  <p className="text-gray-500">No Todos for today.</p>
+                  <NoDataFound title="No Todo Found!" url="pencil.png" />
                 </div>
               )}
 
@@ -145,37 +145,40 @@ const List = () => {
                 </div>
               ) : (
                 <div className="flex flex-col text-center items-center justify-center py-4">
-                  <p className="text-gray-500">No Habits tracked today.</p>
+                  <NoDataFound title="No Habits tracked today" url="dog.png" />
                 </div>
               )}
             </div>
 
-            <Separator />
-
             {/* Today Activity */}
-            <div className="flex justify-between items-center w-full py-4 rounded-lg">
-              <h1 className="flex-grow text-center text-xl ml-6 md:text-2xl font-semibold">
-                Today’s Activity
-              </h1>
-              <Button
-                className="flex items-center gap-2 text-sm md:text-base pl-4 py-2 rounded-lg transition-colors duration-300"
-                variant={"outline"}
-              >
-                <Share className="w-5 h-5" />
-                <span className="hidden md:block">Share</span>
-              </Button>
-            </div>
-
-            {/* Card Box of Today Activity */}
             {data.length > 0 ? (
-              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-6">
-                {data.map((item) => (
-                  <CardBox data={item} key={item._id} />
-                ))}
-              </div>
+              <>
+                {" "}
+                <Separator />
+                <div className="flex justify-between items-center w-full py-4 rounded-lg">
+                  <h1 className="flex-grow text-center text-xl ml-6 md:text-2xl font-semibold">
+                    Today’s Activity
+                  </h1>
+                  <Button
+                    className="flex items-center gap-2 text-sm md:text-base pl-4 py-2 rounded-lg transition-colors duration-300"
+                    variant={"outline"}
+                  >
+                    <Share className="w-5 h-5" />
+                    <span className="hidden md:block">Share</span>
+                  </Button>
+                </div>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-4 mt-6">
+                  {data.map((item) => (
+                    <CardBox data={item} key={item._id} />
+                  ))}
+                </div>
+              </>
             ) : (
               <div className="flex flex-col items-center justify-center py-4">
-                <p className="text-gray-500">No activities found for today.</p>
+                <NoDataFound
+                  title="No activities found for today"
+                  url="hand.png"
+                />
               </div>
             )}
           </div>
