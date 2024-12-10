c87e9e57274dcec1d0196c6d4fb79d7696662cbb
ChandraShekhar <csyadav0513@gmail.com>
2024-12-10 13:47:22 +0530

    Enhance NoDataFound component to accept custom titles and image URLs for better flexibility and user experience

--- client/components/elements/NoDataFound.tsx
+++ client/components/elements/NoDataFound.tsx
@@ -2,31 +2,36 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "../ui/button";
 import { BadgePlus } from "lucide-react";
-import { Separator } from "../ui/separator";
 
-const NoDataFound = () => {
+interface NoDataFoundProps {
+  url?: string;
+  title?: string;
+}
+
+const NoDataFound = ({ url, title }: NoDataFoundProps) => {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full text-center">
-      <Separator />
       <h1 className="text-xl my-2 md:text-2xl mt-4 text-gray-800 dark:text-gray-200 font-semibold flex items-center space-x-2">
-        <span role="img" aria-label="memo">
-          📝
-        </span>
-        <span>No Activity Yet!</span>
-        <span role="img" aria-label="rocket">
-          🚀
-        </span>
+        <span>{title || "No Data Found"}</span>
       </h1>
-      <div className="bg-white rounded-xl">
+      <div className=" rounded-xl">
         <Image
-          src="/no-data.png"
+          src={`/${url || "no-data.png"}`}
+          width={150}
+          height={150}
+          alt="No-Data-Found"
+          className="md:hidden"
+        />
+        <Image
+          src={`/${url || "no-data.png"}`}
           width={300}
           height={300}
           alt="No-Data-Found"
+          className="hidden md:block"
         />
       </div>
 
-      <Button asChild variant={"outline"}>
+      <Button asChild variant={"outline"} size={"sm"}>
         <Link
           href={"/create"}
           className="flex justify-center items-center gap-2 mt-2"
