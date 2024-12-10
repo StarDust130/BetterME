74f7971d85fc6da0b285f88427c8695ca3fdd38d
ChandraShekhar <csyadav0513@gmail.com>
2024-12-10 13:00:50 +0530

    Enhance TodoCards component styling for better visual clarity and improved background color

--- client/components/elements/Cards/TodoCards.tsx
+++ client/components/elements/Cards/TodoCards.tsx
@@ -15,15 +15,15 @@ const TodoCards = ({ todoData, setTodoData }: TodoCardsProps) => {
   };
 
   return (
-    <div className=" p-6 rounded-xl shadow-md w-full md:max-w-4xl md:mx-auto border-b-2 border-t-2">
-      <h2 className="text-xl font-semibold mb-4 text-gray-800 dark:text-gray-200 text-center">
+    <div className=" p-6 rounded-xl bg-gray-100  shadow-md w-full md:max-w-4xl md:mx-auto border-b-2 border-t-2">
+      <h2 className="text-xl font-semibold mb-4 text-black  text-center">
         Today’s Todos
       </h2>
       <ul className="space-y-4">
         {todoData.map((todo, index) => (
           <li
             key={todo._id}
-            className={`flex items-center border justify-between bg-white dark:bg-gray-800 p-4 rounded-lg shadow-sm transition-all duration-300 transform hover:scale-[1.02] ${
+            className={`flex items-center border justify-between bg-gray-50  p-4 rounded-lg shadow-sm transition-all duration-300 transform hover:scale-[1.02] ${
               todo.isCompleted ? "opacity-70" : "opacity-100"
             }`}
           >
@@ -42,7 +42,7 @@ const TodoCards = ({ todoData, setTodoData }: TodoCardsProps) => {
                 className={`text-lg flex-grow transition-colors duration-300 ${
                   todo.isCompleted
                     ? "line-through text-gray-400"
-                    : "text-gray-900 dark:text-gray-100"
+                    : "text-gray-900"
                 }`}
               >
                 {todo.task}
