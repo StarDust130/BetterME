55e373acbeb8e3fdcc1153aadbcda5d32a636821
ChandraShekhar <csyadav0513@gmail.com>
2024-12-11 20:55:43 +0530

    Update CORS configuration to allow requests from any origin

--- server/app.js
+++ server/app.js
@@ -10,14 +10,17 @@ const app = express();
 //! Global Middleware
 
 //? 🛡️ Enable Cross-Origin Resource Sharing (CORS)
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL, // 🌐 Allow requests from CLIENT_URL
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // 🔧 Allowed HTTP methods
-    allowedHeaders: ["Content-Type", "Authorization"], // 🧰 Allowed headers
-    credentials: true, // 🎫 Allow credentials (cookies, headers, etc.)
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL, // 🌐 Allow requests from CLIENT_URL
+//     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"], // 🔧 Allowed HTTP methods
+//     allowedHeaders: ["Content-Type", "Authorization"], // 🧰 Allowed headers
+//     credentials: true, // 🎫 Allow credentials (cookies, headers, etc.)
+//   })
+// );
+
+app.use(cors({ origin: "*" }));
+
 
 app.use(express.json({ limit: "10kb" })); // 📝 Parse JSON requests
 
