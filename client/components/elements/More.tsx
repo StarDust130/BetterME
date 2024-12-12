ffea5dbd045c731496e227b961e78cdf95d1bb85
ChandraShekhar <csyadav0513@gmail.com>
2024-12-12 11:53:59 +0530

    Add delete functionality to More component with toast notifications

--- client/components/elements/More.tsx
+++ client/components/elements/More.tsx
@@ -1,12 +1,46 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useToast } from "@/hooks/use-toast";
+import { getClerkUserID } from "@/lib/action";
+import axios from "axios";
 import { EllipsisVertical, EyeOff, Pencil, Trash2 } from "lucide-react";
 
-const More = () => {
+interface MoreProps {
+  _id: string;
+  field: string;
+}
+
+const More = ({ _id }: MoreProps) => {
+  const { toast } = useToast();
+
+  const handleDelete = async () => {
+    try {
+      const clerkID = await getClerkUserID();
+      const data = await axios.delete(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}?clerkID=${clerkID}&taskId=${_id}&field=todo`
+      );
+
+      console.log("Data 🫥:", data);
+
+      toast({
+        title: "Task Deleted Successfully 🥳",
+        description: `${data?.data?.task} has been deleted successfully.`,
+      });
+    } catch (error: any) {
+      console.log(error);
+      toast({
+        title: "Error",
+        description: error?.response?.data?.message || "Something went wrong",
+        variant: "destructive",
+      });
+    }
+  };
+
   return (
     <div>
       <DropdownMenu>
@@ -22,7 +56,10 @@ const More = () => {
             <EyeOff className="w-4 h-4 text-yellow-500" />
             <span className="text-gray-800  font-medium">Hide Activity</span>
           </DropdownMenuItem>
-          <DropdownMenuItem className="flex items-center dark:hover:bg-red-100 gap-3 px-4 py-2  rounded-md transition-colors duration-300">
+          <DropdownMenuItem
+            className="flex items-center dark:hover:bg-red-100 gap-3 px-4 py-2  rounded-md transition-colors duration-300"
+            onClick={handleDelete}
+          >
             <Trash2 className="w-4 h-4 text-red-500" />
             <span className="text-red-500 font-medium">Delete Permanently</span>
           </DropdownMenuItem>
