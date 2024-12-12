9d801fb05bc45fce228152ea2e9bb89de954725a
ChandraShekhar <csyadav0513@gmail.com>
2024-12-12 16:44:01 +0530

    Update layout component to add margin-bottom for improved spacing in main content area

--- client/app/(root)/layout.tsx
+++ client/app/(root)/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   return (
     <>
       <Header />
-      <main suppressHydrationWarning className="flex md:px-6 py-3 flex-col w-full">
+      <main suppressHydrationWarning className="flex md:px-6 py-3 md:mb-10 flex-col w-full">
         {children}
         <Navbar />
       </main>
