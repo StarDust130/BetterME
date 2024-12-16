3192a3811440912d129f25e322d959c1d062c1d9
ChandraShekhar <csyadav0513@gmail.com>
2024-12-16 11:59:02 +0530

    Add createHabits controller for habit creation functionality

---
+++ server/controllers/habits.controller.js
@@ -0,0 +1,14 @@
+import Habits from "../models/habits.model.js";
+import { AppError } from "../lib/AppError.js";
+import { catchAsync } from "../lib/catchAsync.js";
+
+
+
+
+//! Create 🦆 - Create a new habit
+const createHabits = catchAsync(async (req, res, next) => {});
+
+
+
+
+export { createHabits };
