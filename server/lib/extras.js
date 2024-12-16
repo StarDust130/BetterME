18f05cd2c1dbba2f3a99a89d530003f1557e43fc
ChandraShekhar <csyadav0513@gmail.com>
2024-12-16 13:28:36 +0530

    Add frequency mapping for habit scheduling options

---
+++ server/lib/extras.js
@@ -0,0 +1,8 @@
+
+export const frequencyMap = {
+  "mon-wed-fri": ["mon", "wed", "fri"],
+  "tue-thu-sat": ["tue", "thu", "sat"],
+  weekdays: ["mon", "tue", "wed", "thu", "fri"], // Monday to Friday
+  "mon-sat": ["mon", "tue", "wed", "thu", "fri", "sat"], // Monday to Saturday
+  daily: ["daily"], // Default for every day
+};
