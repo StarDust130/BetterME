0c92bc27ab59e5d16080e88519d668d7d6db81ce
ChandraShekhar <csyadav0513@gmail.com>
2024-12-20 13:07:01 +0530

    Enhance HabitsCards component UI with improved layout, added frequency display, and updated icons

--- client/components/elements/Cards/HabitsCards.tsx
+++ client/components/elements/Cards/HabitsCards.tsx
@@ -1,6 +1,6 @@
-import { getRandomEmoji } from "@/lib/utils";
+import { Capitalized, getRandomEmoji } from "@/lib/utils";
 import { HabitsType } from "../List";
-import {  MoreVertical, CheckCircle, Circle } from "lucide-react"; // Import Lucide icons
+import { MoreVertical, CheckCircle, Circle, Clock } from "lucide-react"; // Import Lucide icons
 
 interface HabitsCardsProps {
   habitsData: HabitsType[];
@@ -8,44 +8,71 @@ interface HabitsCardsProps {
 
 const HabitsCards = ({ habitsData }: HabitsCardsProps) => {
   return (
-    <div className="p-6 rounded-xl shadow-lg w-full md:max-w-4xl md:mx-auto border-t-2 border-b-2 border-gray-300">
+    <div className="p-5 rounded-xl bg-gray-100 shadow-lg w-full md:max-w-4xl md:mx-auto border-b-2 border-t-2">
       <h2 className="text-2xl font-semibold mb-6 text-black text-center">
         Today’s Habits
       </h2>
+
       <ul className="space-y-6">
         {habitsData.map((habit, index) => {
-          // Ensure habit.startDate is a Date object
           const startDate = new Date(habit.startDate);
           const completed = habit.completedDates.includes(
             startDate.toISOString()
           );
+          const frequencyText =
+            habit.frequency.length === 7
+              ? "Daily"
+              : habit.frequency.length === 5
+              ? "Weekdays"
+              : habit.frequency.length === 6
+              ? "Mon-Sat"
+              : Capitalized(habit.frequency.join(", "));
 
           return (
             <li
               key={index}
-              className={`flex items-center justify-between p-5 bg-white rounded-lg shadow-md transition-all duration-300 transform hover:scale-105 ${
+              className={`flex items-center justify-between bg-white p-5 rounded-lg shadow-md transition-all duration-300 transform md:hover:scale-105 ${
                 completed ? "opacity-60" : "opacity-100"
               }`}
             >
-              <div className="flex items-center gap-4 w-full">
-                {/* Checkboxes with icons */}
-                {completed ? (
-                  <CheckCircle className="w-6 h-6 text-green-500 transition-transform duration-300 transform scale-110" />
-                ) : (
-                  <Circle className="w-6 h-6 text-gray-400 transition-transform duration-300 transform scale-90" />
-                )}
-                <div className="flex flex-col flex-grow">
-                  <span
-                    className={`text-lg cursor-pointer transition-colors duration-300 ${
-                      completed ? "line-through text-gray-400" : "text-gray-800"
-                    }`}
-                  >
-                    {habit.habitName}
-                  </span>
+              <div className="flex gap-4 w-full">
+                {/* Circle Icon */}
+                <div className="flex items-center justify-center">
+                  {completed ? (
+                    <CheckCircle className="w-6 h-6 text-green-500" />
+                  ) : (
+                    <Circle className="w-6 h-6 text-gray-400" />
+                  )}
                 </div>
-                <span className="text-sm flex text-gray-500">
-                  {habit.streak} 🔥 {getRandomEmoji()}
-                </span>
+
+                {/* Habit Details */}
+                <div className="flex flex-col w-full justify-center items-start flex-grow">
+                  {/* Habit Name and Streak */}
+                  <div className="flex justify-between items-center w-full">
+                    <span
+                      className={`text-xl font-semibold cursor-pointer transition-colors duration-300 ${
+                        completed
+                          ? "line-through text-gray-400"
+                          : "text-gray-800"
+                      }`}
+                    >
+                      {habit.habitName}
+                    </span>
+
+                    <div className="mt-2 text-sm text-gray-700 flex justify-center items-center">
+                      <span className="font-semibold">{habit.streak}</span> 🔥{" "}
+                      {getRandomEmoji()}
+                    </div>
+                  </div>
+
+                  {/* Frequency  */}
+                  <div className="mt-2 flex items-center text-sm  rounded-lg text-gray-500">
+                    <Clock className="w-5 h-5 text-blue-500" />
+                    <span className="ml-2 text-gray-800">{frequencyText}</span>
+                  </div>
+                </div>
+
+                {/* More Icon */}
                 <span>
                   <MoreVertical className="w-5 h-5 text-gray-600 cursor-pointer hover:text-gray-900" />
                 </span>
