7f22e66783cba68d77eab70977d218289b9cd099
ChandraShekhar <csyadav0513@gmail.com>
2024-12-24 19:12:39 +0530

    Refactor frequencyMap to remove unused frequency options for improved clarity

--- server/lib/extras.js
+++ server/lib/extras.js
@@ -1,8 +1,6 @@
 export const frequencyMap = {
   "mon-wed-fri": ["mon", "wed", "fri"],
   "tue-thu-sat": ["tue", "thu", "sat"],
-  weekdays: ["mon", "tue", "wed", "thu", "fri"], // Monday to Friday
-  "mon-sat": ["mon", "tue", "wed", "thu", "fri", "sat"], // Monday to Saturday
   daily: ["mon", "tue", "wed", "thu", "fri", "sat", "sun"], // All days
 };
 
