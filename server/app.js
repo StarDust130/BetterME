21b5eceaf15d02cb3ae047259b921ebe47734842
ChandraShekhar <csyadav0513@gmail.com>
2024-12-26 13:30:46 +0530

    Add stats.routes.js to integrate statistics routes into the application

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dayTaskRoutes from "./routes/dayTask.routes.js"; // 📅 DayTask Routes
 import habitsRouter from "./routes/habits.routes.js"; // 🤑 Habits Routes
+import statsRouter from "./routes/stats.routes.js"; // 📊 Stats Routes
 import { AppError } from "./lib/AppError.js";
 import { globalErrorHandler } from "./controllers/error.controller.js";
 import "dotenv/config";
@@ -28,6 +29,7 @@ app.use(express.json({ limit: "10kb" })); // 📝 Parse JSON requests
 //! Routes 🎠
 app.use("/api/v1/dayTask", dayTaskRoutes); // 📅 DayTask Routes
 app.use("/api/v1/habits", habitsRouter); // 🤑 Habits Routes
+app.use("/api/v1/stats", statsRouter); // 📊 Stats Routes
 
 
 //! 404 Handler
