4ca8ae305f8b68e96c4ab92cbc5e03d0f685889f
ChandraShekhar <csyadav0513@gmail.com>
2024-12-28 15:19:00 +0530

    Add DatePicker component and enhance stats page to greet users

--- client/app/(root)/stats/page.tsx
+++ client/app/(root)/stats/page.tsx
@@ -1,8 +1,21 @@
-import StatsPage from "@/components/elements/stats/StatsPage"
+import DatePicker from "@/components/elements/stats/DatePicker";
+import StatsPage from "@/components/elements/stats/StatsPage";
+import { currentUser } from "@clerk/nextjs/server";
 
-const Page = () => {
+const Page = async () => {
+  const user = await currentUser();
   return (
-    <StatsPage />
-  )
-}
-export default Page
+    <main className="flex flex-col  items-center w-full  p-4 min-h-screen">
+      <div className="w-full max-w-3xl p-6 rounded-lg bg-gray-100  text-black dark:shadow-gray-500 dark:shadow-sm  shadow-xl">
+        <h1 className="text-xl font-bold w-full  text-left mb-6 md:text-4xl">
+          Hi {user?.firstName || "Guest"}! 😉
+        </h1>
+
+        <DatePicker />
+      </div>
+
+      <StatsPage />
+    </main>
+  );
+};
+export default Page;
