9d129656f215aacfa21f7eacc86afb2926e2475a
ChandraShekhar <csyadav0513@gmail.com>
2025-01-02 17:17:25 +0530

    Add calculateMonthlyStats function to aggregate monthly expenses and junk food totals

--- server/lib/aggregation.js
+++ server/lib/aggregation.js
@@ -221,3 +221,30 @@ export const habitsProgressStats = async (clerkID, timeframe) => {
 
 
 
+export const calculateMonthlyStats = async (clerkID, date) => {
+  const startOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
+  const endOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+
+  const monthlyStats = await DayTask.aggregate([
+    {
+      $match: {
+        clerkID,
+        date: { $gte: startOfMonth, $lte: endOfMonth },
+      },
+    },
+    {
+      $project: {
+        expensesTotal: { $sum: "$expenses.amount" },
+        junkFoodTotal: { $sum: "$junkFood.amount" },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        total: { $sum: { $add: ["$expensesTotal", "$junkFoodTotal"] } },
+      },
+    },
+  ]);
+
+  return monthlyStats[0] || { total: 0 };
+};
