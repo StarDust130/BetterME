16a05ca9f7e84683bd763c16fbe6cccc4b627acf
ChandraShekhar <csyadav0513@gmail.com>
2025-01-02 15:05:08 +0530

    Add dark mode border styling to Card component

--- client/components/ui/card.tsx
+++ client/components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-xl  bg-card text-card-foreground shadow",
+      "rounded-xl  bg-card dark:border-gray-700 dark:border text-card-foreground shadow",
       className
     )}
     {...props}
