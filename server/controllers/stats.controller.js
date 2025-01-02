06e4a7e84aba57ed0a847c41f43fe78192ac3ded
ChandraShekhar <csyadav0513@gmail.com>
2025-01-02 11:37:10 +0530

    Refactor stats controller: remove unused functions and add placeholders for new stats endpoints

--- server/controllers/stats.controller.js
+++ server/controllers/stats.controller.js
@@ -1,79 +1,21 @@
-import {
-  getDayTaskStats,
-  getHabitsStats,
-  getTodosCompletionStats,
-  habitsProgressStats,
-} from "../lib/aggregation.js";
+
 import { AppError } from "../lib/AppError.js";
 import { catchAsync } from "../lib/catchAsync.js";
 import DayTask from "../models/dayTask.models.js";
 
-//! Habit Progress - GET /api/stats/habit-progress
-// Purpose: Fetch streaks and completion rates for all habits.
-// [
-//   { "habitName": "Read 10 pages", "streak": 5, "completionRate": 80 },
-//   { "habitName": "Exercise", "streak": 3, "completionRate": 60 }
-// ]
+//! Expenes Stats 🤑
+const ExpenesStats = catchAsync(async (req, res, next) => {});
 
-//! Todos Completion Stats 🛺
-const OverviewStats = catchAsync(async (req, res) => {
-  const clerkID = req.clerkID;
-  const { timeframe = "all" } = req.query;
+//! JunkFood Stats 🍔
+const JunkFoodStats = catchAsync(async (req, res, next) => {});
 
-  const [dayTaskStats, habitsStats] = await Promise.all([
-    getDayTaskStats(clerkID, timeframe),
-    getHabitsStats(clerkID, timeframe),
-  ]);
+//! Todos Stats 📝
+const TodosStats = catchAsync(async (req, res, next) => {});
 
-  return res.json({
-    totalExpenses: dayTaskStats.totalExpenses,
-    junkFoodCount: dayTaskStats.junkFoodCount,
-    todosCompleted: dayTaskStats.todosCompleted,
-    habitsCompleted: habitsStats.habitsCompleted,
-  });
-});
+//! Habits Stats 😉
+const HabitsStats = catchAsync(async (req, res, next) => {});
 
-//! Expenses vs Junk Trend 💪
-const ExpensesVsJunkTrend = catchAsync(async (req, res) => {
-  const clerkID = req.clerkID;
-  const { timeframe = "all" } = req.query;
+//!  Day Wise  Stats 📅
+const allDayStats = catchAsync(async (req, res, next) => {});
 
-  const trendData = await getDayTaskStats(clerkID, timeframe, true);
-
-  return res.json({
-    trendData,
-  });
-});
-
-const HabitsProgress = catchAsync(async (req, res, next) => {
-  const clerkID = req.clerkID;
-  const { timeframe = "all" } = req.query;
-
-  const habitsProgress = await habitsProgressStats(clerkID, timeframe);
-
-  return res.json({
-    habitsProgress,
-  });
-});
-
-//! Todos Completion Stats 👩‍🚀
-const TodosCompletionStats = catchAsync(async (req, res, next) => {
-  const clerkId = req.clerkID;
-  const { timeframe = "all" } = req.query;
-
-  const todoStats = await getTodosCompletionStats(clerkId, timeframe);
-
-  return res.json({
-    todoStats,
-  });
-});
-
-const DailyLogs = catchAsync(async (req, res, next) => {});
-
-export {
-  OverviewStats,
-  ExpensesVsJunkTrend,
-  HabitsProgress,
-  TodosCompletionStats,
-  DailyLogs,
-};
+export { ExpenesStats, JunkFoodStats, TodosStats, HabitsStats, allDayStats };
