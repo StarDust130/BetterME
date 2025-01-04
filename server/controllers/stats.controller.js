d7b0907033d7b323b05dd99eba886fcb94587f47
ChandraShekhar <csyadav0513@gmail.com>
2025-01-04 20:52:04 +0530

    Fix todo stats controller

--- server/controllers/stats.controller.js
+++ server/controllers/stats.controller.js
@@ -3,7 +3,11 @@ import { catchAsync } from "../lib/catchAsync.js";
 import { getDateFilter } from "../lib/extras.js";
 
 import getSummaryAndTips from "../ai/getSummaryAndTips.js";
-import { expensesStatsAggregation, junkFoodStatsAggregation } from "../lib/aggregation.js";
+import {
+  expensesStatsAggregation,
+  junkFoodStatsAggregation,
+  todosStatsAggregation,
+} from "../lib/aggregation.js";
 
 //! Expenses Stats 💸
 const ExpensesStats = catchAsync(async (req, res, next) => {
@@ -61,27 +65,29 @@ const JunkFoodStats = catchAsync(async (req, res, next) => {
 
 //! Todos Stats 📝
 const TodosStats = catchAsync(async (req, res, next) => {
-    const clerkID = req.clerkID;
-    const { timeframe } = req.query;
+  const clerkID = req.clerkID;
+  const { timeframe } = req.query;
 
-    // Get the date filter based on the selected timeframe
-    const dateFilter = getDateFilter(timeframe);
+  // Get the date filter based on the selected timeframe
+  const dateFilter = getDateFilter(timeframe);
 
-    const stats = await todosStatsAggregation(clerkID, dateFilter);
+  const stats = await todosStatsAggregation(clerkID, dateFilter);
 
-    // const summary = await getSummaryAndTips(aiData, "todos");
+  // const summary = await getSummaryAndTips(aiData, "todos");
 
-    const responseData = {
-        insights: {
-            ...stats,
-            summary: {
-                summary:
-                    "You spent a total of ₹6000 in 1 day, with ₹5000 on essentials 📊 and ₹1000 on junk food 😊. Your average daily spend is ₹6000.",
-                recommendation:
-                    "Consider reducing junk food expenses, as ₹1000 is a significant amount for a single day. Allocate that amount to savings or essential expenses for a more balanced budget.",
-            },
-        },
-    };
+  const responseData = {
+    insights: {
+      ...stats,
+      summary: {
+        summary:
+          "You spent a total of ₹6000 in 1 day, with ₹5000 on essentials 📊 and ₹1000 on junk food 😊. Your average daily spend is ₹6000.",
+        recommendation:
+          "Consider reducing junk food expenses, as ₹1000 is a significant amount for a single day. Allocate that amount to savings or essential expenses for a more balanced budget.",
+      },
+    },
+  };
+
+  res.status(200).json(responseData);
 });
 
 //! Habits Stats 😉
